316f608f5bcc44bc23cdd6054da6b044d458a385
zijun <zqi3ug@gmail.com>
2025-12-25 15:58:31 +0800

    更新模板设计模式

--- Chapter-9/src/com/hspedu/P06_abstract/TestTemplate.java
+++ Chapter-9/src/com/hspedu/P06_abstract/TestTemplate.java
@@ -3,42 +3,45 @@ package com.hspedu.P06_abstract;
 public class TestTemplate {
     public static void main(String[] args) {
         //
-        new AA().job();
-        new BB().job();;
+        new AA().calculateTime();
+        new BB().calculateTime();
     }
 }
 
-
-
-class AA {
-    // 计算任务
+class AA extends Template {
+    // 实现 template 的抽象方法 job
+    @Override
     public void job() {
         long num = 0;
-        // 得到开始时间
-        long start = System.currentTimeMillis();
         for (int i = 1; i <= 800000; i++) {
             num += i;
         }
-        // 得到结束时间
-        long end = System.currentTimeMillis();
-        System.out.println("执行时间 = "+ (end - start)  + " ms" );
+    }
+}
 
+class BB extends Template{
+    // 实现 template 的抽象方法 job
+    @Override
+    public void job() {
+        long num = 0;
+        for (int i = 1; i <= 80000; i++) {
+            num *= i;
+        }
     }
 }
 
 
-class BB {
-    // 计算任务
-    public void job() {
-        long num = 0;
+abstract class Template {
+    // 统计执行时间
+    public void calculateTime() {
         // 得到开始时间
         long start = System.currentTimeMillis();
-        for (int i = 1; i <= 8000; i++) {
-            num *= i;
-        }
+        // 执行 job
+        job();
         // 得到结束时间
         long end = System.currentTimeMillis();
-        System.out.println("执行时间 = "+ (end - start)  + " ms" );
-
+        System.out.println("任务执行时间 = "+ (end - start)  + " ms" );
     }
+    // 抽象方法
+    public abstract void job();
 }
